b33f1bba01cea86736db7ab602a8029c46633125
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2021-10-29 20:15:54 +0530

    Shortcode plugin

---
+++ Shortcode plugin/main plugin.php
@@ -0,0 +1,21 @@
+<?php
+Plugin Name:shortcode example
+Plugin URI: https://example.com/plugin-name
+Description: Description of the plugin.
+Version: 1.0.0
+Author: Abhilasha
+Author URI: https://example.com
+Text Domain: plugin-name
+
+add_shortcode('wordpress_contact_form','render_wordpress_contact_form');
+
+function render_wordpress_contact_form(){?>
+<form>
+First name:<br>
+<input type="text" name="firstname" value="Joe">
+<br>
+<input type="submit" value="Send">
+</form>
+<?php
+
+}
