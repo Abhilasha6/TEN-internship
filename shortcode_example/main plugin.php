017f56d492feae61b6334f5dfb999c3f1f06aadf
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2021-10-29 19:41:44 +0530

    CORE TASK 04
    CREATING A SHORTCODE PLUGIN IN WORDPRESS

---
+++ shortcode_example/main plugin.php
@@ -0,0 +1,8 @@
+<?php
+Plugin Name: shortcode_example
+Plugin URI: https://example.com/shortcode_example
+Description: Description of the plugin.
+Version: 1.0.0
+Author: Abhilasha
+Author URI: https://example.com
+Text Domain: plugin-name
